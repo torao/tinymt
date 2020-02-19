062f9d91dae54f27c472b59d6124bb2399732df4
TAKAMI Torao <koiroha@gmail.com>
2020-02-20 00:29:58 +0900

    add random string future to CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,26 +4,33 @@ extern crate tinymt;
 
 use clap::{App, Arg};
 use rand::{Rng, RngCore, SeedableRng};
-use std::num::ParseIntError;
 use std::process;
+use std::str::FromStr;
 use tinymt::{TinyMT32, TinyMT64};
 
-fn rand32(seed: Option<&str>) -> TinyMT32 {
-  if let Some(seed) = seed {
-    let seed: u32 = seed.parse().unwrap();
+/// Parse the specified string to integer.
+fn num<F: FromStr>(s: &str) -> Result<F, String> where <F as std::str::FromStr>::Err: std::fmt::Display {
+  FromStr::from_str(s).map_err(|e: F::Err| { format!("ERROR: {}: {}", e, s) })
+}
+
+/// Generate TinyMT32 with specified seed.
+fn rand32(seed: Option<&str>) -> Result<TinyMT32, String> {
+  Ok(if let Some(seed) = seed {
+    let seed: u32 = num(seed)?;
     TinyMT32::from_seed_u32(seed)
   } else {
     TinyMT32::from_entropy()
-  }
+  })
 }
 
-fn rand64(seed: Option<&str>) -> TinyMT64 {
-  if let Some(seed) = seed {
-    let seed: u64 = seed.parse().unwrap();
+/// Generate TinyMT64 with specified seed.
+fn rand64(seed: Option<&str>) -> Result<TinyMT64, String> {
+  Ok(if let Some(seed) = seed {
+    let seed: u64 = num(seed)?;
     TinyMT64::from_seed_u64(seed)
   } else {
     TinyMT64::from_entropy()
-  }
+  })
 }
 
 fn main() {
@@ -36,32 +43,38 @@ fn main() {
 fn exec() -> Result<(), String> {
   let args = arguments().get_matches();
   let seed = args.value_of("seed");
-  let n = args.value_of("count").unwrap().parse()
-    .map_err(|e: ParseIntError| { format!("ERROR: {}", e.to_string()) })?;
+  let n: usize = num(args.value_of("count").unwrap())?;
   match args.value_of("type").unwrap() {
     "int" | "int32" | "i32" | "u32" | "32" =>
-      print(&mut rand32(seed), n, |r| { format!("{}", r.next_u32()) }),
+      print(&mut rand32(seed)?, n, |r| { format!("{}", r.next_u32()) }),
     "long" | "int64" | "i64" | "u64" | "64" =>
-      print(&mut rand64(seed), n, |r| { format!("{}", r.next_u64()) }),
+      print(&mut rand64(seed)?, n, |r| { format!("{}", r.next_u64()) }),
     "float" | "float32" | "f32" =>
-      print(&mut rand32(seed), n, |r| { format!("{}", r.gen_range(0f32, 1f32)) }),
+      print(&mut rand32(seed)?, n, |r| { format!("{}", r.gen_range(0f32, 1f32)) }),
     "double" | "float64" | "f64" =>
-      print(&mut rand64(seed), n, |r| { format!("{}", r.gen_range(0f64, 1f64)) }),
+      print(&mut rand64(seed)?, n, |r| { format!("{}", r.gen_range(0f64, 1f64)) }),
+    "string" | "str" | "s" => {
+      let mut r = rand64(seed)?;
+      let length: usize = num(args.value_of("length").unwrap())?;
+      let radix: Vec<char> = args.value_of("radix").unwrap().chars().collect();
+      for _ in 0..n {
+        println!("{}", (0..length).map(|_| { radix[r.gen_range(0, radix.len())] }).collect::<String>());
+      }
+    }
     unknown =>
       return Err(format!("ERROR: the specified type is not defined: {}", unknown))
   };
   Ok(())
 }
 
-fn print<T>(rng: &mut T, n: usize, rand: fn(&mut T) -> String) {
+fn print<T>(rng: &mut T, n: usize, next_random: fn(&mut T) -> String) {
   for _ in 0..n {
-    println!("{}", rand(rng));
+    println!("{}", next_random(rng));
   }
 }
 
 fn error(message: String) -> ! {
   eprintln!("{}", message);
-  arguments().print_help().unwrap();
   process::exit(1)
 }
 
@@ -81,7 +94,21 @@ fn arguments() -> App<'static, 'static> {
       .long("type")
       .takes_value(true)
       .default_value("double")
-      .help("generate random number of int, long, float or double type")
+      .help("generate random number of int, long, float, double or string type")
+    )
+    .arg(Arg::with_name("length")
+      .short("l")
+      .long("length")
+      .takes_value(true)
+      .default_value("32")
+      .help("the number of characters when generating random strings")
+    )
+    .arg(Arg::with_name("radix")
+      .short("r")
+      .long("radix")
+      .takes_value(true)
+      .default_value("0123456789abcdefghijklmnopqrstuvwxyz")
+      .help("characters to be used when generating random string")
     )
     .arg(Arg::with_name("seed")
       .help("seed of pseudo-random number generator")
